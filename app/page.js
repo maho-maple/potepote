7b369fc347fc1376ecad097cc1d13eb8ff876132
maho-maple <maho.statice@gmail.com>
2024-02-06 22:15:36 +0900

    Button componenntを作成

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+import Button from "./_components/button"
+
 export default function Home() {
   return(
    <div>
@@ -5,11 +7,18 @@ export default function Home() {
       ぽてぽて
     </p>
     <ul>
-      <li>とり天</li>
+      <li>とり天</li> 
       <li>唐揚げ</li>
       <li>温泉たまご</li>
       <li>プリン</li>
     </ul>
+
+  
+    <Button message="承認"/>
+
+    <Button message="保留"/>
+    <Button message="却下"/>
+    <Button message="ぽてぽて"/>
     </div> 
   )
 }
